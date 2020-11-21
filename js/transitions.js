512b15857714f82d4f33a34dce75ea8d746ad44e
Sergey Doroshenko <SDarashenka@gomel.iba.by>
2020-11-21 18:05:22 +0300

    Add the new images and smooth transitions

---
+++ js/transitions.js
@@ -0,0 +1,15 @@
+$(document).on('click', 'a[href^="#one"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
+
+$(document).on('click', 'a[href^="#contacts-footer"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
